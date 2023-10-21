842d586f733e9b1238133e9e3d6100533a506045
Joonas Miettinen <joonas.miettinen@gmail.com>
2023-10-21 14:38:41 +0300

    feat: add an overload for update, lpmf

--- src/ngm_to_ngmpy.cpp
+++ src/ngm_to_ngmpy.cpp
@@ -6,7 +6,30 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(ngmpy, m) {
     py::class_<ngram::NgramModel>(m, "NgramModel")
-        .def(py::init<int, double, double, bool>())
-        .def("update", py::overload_cast<const std::vector<std::string>&>(&ngram::NgramModel::update))
-        .def("lpmf", static_cast<std::vector<double> (ngram::NgramModel::*)(const std::vector<std::string>&, int) const>(&ngram::NgramModel::lpmf));
+          .def(
+            py::init<int, double, double, bool>(),
+            py::arg("n"), py::arg("alpha"),
+            py::arg("unseen_alpha"), py::arg("normalise_length")
+        )
+        .def(
+            "update",
+            py::overload_cast<const std::vector<std::string>&>(&ngram::NgramModel::update),
+            py::arg("texts")
+        )
+        .def(
+            "update",
+            py::overload_cast<const std::string&>(&ngram::NgramModel::update),
+            py::arg("text")
+        )
+        .def(
+            "lpmf",
+            static_cast<std::vector<double> (ngram::NgramModel::*)(const std::vector<std::string>&, int) const>(&ngram::NgramModel::lpmf),
+            py::arg("texts"), py::arg("n_threads")
+        )
+        .def(
+            "lpmf",
+            static_cast<double (ngram::NgramModel::*)(const std::string&) const>(&ngram::NgramModel::lpmf),
+            py::arg("text")
+        )
+        ;
 }
